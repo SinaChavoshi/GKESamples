68f481f527860ea19f72081f34f13dcd5503d9ed
Sina Chavoshi <sina.chavoshi@gmail.com>
2025-06-30 21:21:54 +0000

    wip jax

--- setup_env_vars.sh
+++ setup_env_vars.sh
@@ -4,19 +4,20 @@ export CLUSTER_NAME="tpu-cluster"
 export AR_REPO_NAME="tpu-repo"
 export BUCKET_NAME="lora-finetuning-data-${PROJECT_ID}"
 export K8S_NAMESPACE="default"
+export JOB_NAME="lora-finetuning-job-jax-4x4"
 
 # docker image build and push 
-export IMAGE_URI="${REGION}-docker.pkg.dev/${PROJECT_ID}/${AR_REPO_NAME}/tpu-lora-trainer-jax:v1"
+export IMAGE_URI="${REGION}-docker.pkg.dev/${PROJECT_ID}/${AR_REPO_NAME}/tpu-lora-trainer-jax:v3"
 docker build . -t ${IMAGE_URI}
 docker push ${IMAGE_URI}
 
 # reload job 
-kubectl delete job lora-finetuning-job
+kubectl delete job ${JOB_NAME}
 kubectl apply -f tpu-training-job.yaml
 
 kubectl get jobs
 kubectl get pods
-kubectl logs -f job/lora-finetuning-job
+kubectl logs -f job/${JOB_NAME}
 
 gcloud storage ls gs://${BUCKET_NAME}/output/final_checkpoint/
 
