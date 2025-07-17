7e8635e5ac081c56274820d568cf4fc5df29328d
Sina Chavoshi <sina.chavoshi@gmail.com>
2025-07-17 19:40:58 +0000

    WIP

--- dlrm-dncv2/RecML/build_and_run.sh
+++ dlrm-dncv2/RecML/build_and_run.sh
@@ -8,7 +8,8 @@ export IMAGE_TAG="latest"
 export BUCKET_NAME="dlrm-training-${PROJECT_ID}"
 export IMAGE_NAME="jax-dlrm-gke"
 export ZONE="us-east5-a"
-export TPU_NAME="v6e16"
+# export TPU_NAME="v6e16"
+export TPU_NAME="chavoshi-dlrm-dnc-v2-benchmark"
 
 docker build -t us-east5-docker.pkg.dev/chavoshi-gke-dev/tpu-repo/jax-dlrm-gke:latest .
 docker push us-east5-docker.pkg.dev/chavoshi-gke-dev/tpu-repo/jax-dlrm-gke:latest
@@ -22,3 +23,6 @@ kubectl get pods -l jobset.sigs.k8s.io/jobset-name=jax-dlrm-benchmark-v6e-32chip
 sleep 10
 
 kubectl logs -f jobs/jax-dlrm-benchmark-v6e-32chip-worker-0
+
+# Pusher to update the remote workers
+./sync_specific_files_scp.sh  dlrm_main.py 
