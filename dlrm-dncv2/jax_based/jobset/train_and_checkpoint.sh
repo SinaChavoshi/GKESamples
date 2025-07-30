0757ea6ca48af724d7d7e763261d69d993fcde13
Sina Chavoshi <sina.chavoshi@gmail.com>
2025-07-30 18:30:46 +0000

    wip

--- dlrm-dncv2/jax_based/jobset/train_and_checkpoint.sh
+++ dlrm-dncv2/jax_based/jobset/train_and_checkpoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-
-export LIBTPU_INIT_ARGS=
-export XLA_FLAGS=
-
-export TPU_NAME=<TPU_NAME>
+set -ex
 export LEARNING_RATE=0.0034
-export BATCH_SIZE=4224
+export BATCH_SIZE=16896
 export EMBEDDING_SIZE=128
-export MODEL_DIR=/tmp/
+export MODEL_DIR=/gcs/benchmark/model-output/v6e-16
 export FILE_PATTERN=gs://qinyiyan-vm/mlperf-dataset/criteo_merge_balanced_4224/train-*
 export NUM_STEPS=28000
 export CHECKPOINT_INTERVAL=1500
@@ -20,18 +16,18 @@ export EMBEDDING_THRESHOLD=21000
 export LOGGING_INTERVAL=1500
 export RESTORE_CHECKPOINT=true
 
-python recml/inference/models/jax/DLRM_DCNv2/dlrm_main.py \
---learning_rate=${LEARNING_RATE} \
---batch_size=${BATCH_SIZE} \
---embedding_size=${EMBEDDING_SIZE} \
---embedding_threshold=${EMBEDDING_THRESHOLD} \
---model_dir=${MODEL_DIR} \
---file_pattern=${FILE_PATTERN} \
---num_steps=${NUM_STEPS} \
---save_checkpoint_interval=${CHECKPOINT_INTERVAL} \
---restore_checkpoint=${RESTORE_CHECKPOINT} \
---eval_interval=${EVAL_INTERVAL} \
---eval_file_pattern=${EVAL_FILE_PATTERN} \
---eval_steps=${EVAL_STEPS}  \
---mode=${MODE} \
---logging_interval=${LOGGING_INTERVAL}
+python /app/RecML/recml/inference/models/jax/DLRM_DCNv2/dlrm_main.py \
+  --learning_rate=${LEARNING_RATE} \
+  --batch_size=${BATCH_SIZE} \
+  --embedding_size=${EMBEDDING_SIZE} \
+  --embedding_threshold=${EMBEDDING_THRESHOLD} \
+  --model_dir=${MODEL_DIR} \
+  --file_pattern=${FILE_PATTERN} \
+  --num_steps=${NUM_STEPS} \
+  --save_checkpoint_interval=${CHECKPOINT_INTERVAL} \
+  --restore_checkpoint=${RESTORE_CHECKPOINT} \
+  --eval_interval=${EVAL_INTERVAL} \
+  --eval_file_pattern=${EVAL_FILE_PATTERN} \
+  --eval_steps=${EVAL_STEPS}  \
+  --mode=${MODE} \
+  --logging_interval=${LOGGING_INTERVAL}
